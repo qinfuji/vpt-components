7e5e625f5b6e55a2da2026e9d6260341a5d8a81f
qinfuji <7748112@qq.com>
2018-06-03 00:42:30 +0800

    add less module

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -12,7 +12,14 @@ module.exports = {
 				test: /\.less/,
 				use: [
 					{ loader: 'style-loader' },
-					{ loader: 'css-loader' },
+					{
+						loader:
+							'css-loader' /*,
+						options: {
+							modules: true,
+							localIndexName: '[name]__[local]___[hash:base64:5]'
+						}*/
+					},
 					{ loader: 'less-loader' }
 				]
 			},
