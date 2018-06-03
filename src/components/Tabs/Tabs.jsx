76622a9d60ad613b152f390707e09c6adc5335f8
qinfuji <7748112@qq.com>
2018-06-03 23:09:34 +0800

    dev tabs component

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,12 +1,26 @@
 import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
+import classname from 'classnames';
 import './Tabs.less';
 
 class Tabs extends React.Component {
+	componentDidMount() {}
+
 	render() {
+		let { style, orientation, position, size } = this.props;
+		let cx = classname(
+			'tabs',
+			size,
+			//'expand',
+			'tabs-expand-fs',
+			'flex-justify-center mt-2',
+			position,
+			orientation
+		);
 		return (
-			<div className="tabs tabs-expand-md">
+			<div className={cx} style={style}>
+				<div className="expand-title">Home</div>
 				<ul>
 					<li className="active">
 						<a href="#">文件</a>
@@ -21,6 +35,11 @@ class Tabs extends React.Component {
 						<a href="#">帮助</a>
 					</li>
 				</ul>
+				<button className="hamburger menu-down dark" type="button">
+					<span className="line" />
+					<span className="line" />
+					<span className="line" />
+				</button>
 			</div>
 		);
 	}
@@ -28,8 +47,40 @@ class Tabs extends React.Component {
 
 Tabs.displayName = 'Tabs';
 
-Tabs.propTypes = {};
+Tabs.propTypes = {
+	/**
+	 * tab orientation
+	 */
+	orientation: PropTypes.oneOf(['vertical', 'horizontal']),
+	/**
+	 * 当orientation为horizontal ， 可以选择top,bottom
+	 * 当orientation为vertical ，可以选择left,right
+	 */
+	position: PropTypes.oneOf(['top', 'left', 'right', 'bottom']),
+	/**
+	 * 当tab选择发生变换时触发
+	 */
+	onChange: PropTypes.func,
+	/**
+	 * 选项卡点击时发生
+	 */
+	onClick: PropTypes.func,
 
-Tabs.defaultProps = {};
+	/**
+	 * style
+	 */
+	style: PropTypes.object,
+
+	/**
+	 * tab szie
+	 */
+	size: PropTypes.oneOf(['sm', 'md', 'lg', 'xl'])
+};
+
+Tabs.defaultProps = {
+	orientation: 'horizontal',
+	position: 'top',
+	size: 'md'
+};
 
 export default Tabs;
--- src/components/Tabs/Tabs.story.js
+++ src/components/Tabs/Tabs.story.js
@@ -1,16 +1,43 @@
 import React from 'react';
-import { text, select, boolean, object } from '@storybook/addon-knobs';
-import { optionalSelect } from '../../util/optionalSelect';
+import { select, object } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import readme from './README.md';
 import Tabs from './Tabs';
 import '../../index.less';
 
 const component = () => {
+	let style = {
+		//fontSize: '.5rem'
+	};
+
+	let orientation = {
+		horizontal: 'horizontal',
+		vertical: 'vertical'
+	};
+
+	let position = {
+		top: 'top',
+		left: 'left',
+		bottom: 'bottom',
+		right: 'right'
+	};
+
+	let size = {
+		sm: 'sm',
+		md: 'md',
+		lg: 'lg',
+		xl: 'xl'
+	};
+
 	return (
-		<div style={{ width: '800px' }}>
-			<Tabs />
-		</div>
+		<Tabs
+			onChange={action('tab-change')}
+			onClick={action('tab-click')}
+			orientation={select('orientation', orientation, 'horizontal')}
+			position={select('position', position, 'top')}
+			size={select('size', size, 'md')}
+			style={object('style', style)}
+		/>
 	);
 };
 
